67f1576863f37f03564277633061165b4c769cea
Jonatas Santos <jonatassantos96@hotmail.com>
2022-06-06 21:26:39 -0300

    💄 Create layout for sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,28 +1,53 @@
 import React from "react";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
 
 import Input from "@components/Input";
 import Button from "@components/Button";
 
-import { Container } from "./styles";
+import BrandImg from "@assets/brand.png";
+
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordButtonLabel,
+} from "./styles";
 
 const SignIn = () => {
+  const behavior = Platform.OS === "ios" ? "padding" : undefined;
+
   return (
     <Container>
-      <Input
-        placeholder={"E-mail"}
-        type={"secondary"}
-        autoCorrect={false}
-        autoCapitalize={"none"}
-      />
+      <KeyboardAvoidingView behavior={behavior} keyboardVerticalOffset={25}>
+        <Content>
+          <Brand source={BrandImg} />
 
-      <Input
-        placeholder={"Senha"}
-        type={"secondary"}
-        autoCorrect={false}
-        secureTextEntry
-      />
+          <Title>Login</Title>
+          <Input
+            placeholder={"E-mail"}
+            type={"secondary"}
+            autoCorrect={false}
+            autoCapitalize={"none"}
+          />
 
-      <Button title={"Entrar"} type={"secondary"} />
+          <Input
+            placeholder={"Senha"}
+            type={"secondary"}
+            autoCorrect={false}
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordButtonLabel>
+              Esqueci minha senha
+            </ForgotPasswordButtonLabel>
+          </ForgotPasswordButton>
+
+          <Button title={"Entrar"} type={"secondary"} />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 };
