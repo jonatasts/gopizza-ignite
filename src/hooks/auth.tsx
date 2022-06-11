89a9a86cfa88041f5ab0cccc512baf13cf90fc8d
Jonatas Santos <jonatassantos96@hotmail.com>
2022-06-11 15:39:55 -0300

    ✨ Add sign in method on context

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,12 @@
-import React, { createContext, useContext, ReactNode } from "react";
+import React, { createContext, useContext, ReactNode, useState } from "react";
+import { Alert } from "react-native";
 
-type AuthContextData = {};
+import auth from "@react-native-firebase/auth";
+
+type AuthContextData = {
+  singIn: (email: string, password: string) => Promise<void>;
+  isLoginIn: boolean;
+};
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -9,7 +15,44 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [isLoginIn, setIsLoginIn] = useState(false);
+
+  const singIn = async (email: string, password: string) => {
+    if (!email || !password) {
+      return Alert.alert("Informe o e-mail e a senha!");
+    }
+
+    setIsLoginIn(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log("Login efetuado com sucesso!", user);
+      })
+      .catch((error) => {
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "auth/invalid-email" ||
+          error.code === "auth/wrong-password"
+        ) {
+          return Alert.alert("Login", "E-mail ou senha inválidos!");
+        } else {
+          return Alert.alert("Login", "Não foi possível realizar o login!");
+        }
+      })
+      .finally(() => setIsLoginIn(false));
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        singIn,
+        isLoginIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 const useAuth = () => {
