a6518d77c8e2fe2fc72bf63de82ee570c68f69fb
Jonatas Santos <jonatassantos96@hotmail.com>
2022-06-15 20:38:55 -0300

    ✨ Create product screen

---
+++ src/screens/Product/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Platform, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components/native";
+
+import { Container, Header, Title } from "./styles";
+
+const Product = () => {
+  const behavior = Platform.OS === "ios" ? "padding" : undefined;
+  const { COLORS } = useTheme();
+
+  return (
+    <Container behavior={behavior}>
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <MaterialCommunityIcons
+            name={"trash-can-outline"}
+            size={24}
+            color={COLORS.TITLE}
+          />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
+};
+
+export default Product;
---
+++ src/screens/Product/styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+}))`
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+
+  padding: ${getStatusBarHeight() + 33}px 20px 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
+`;
