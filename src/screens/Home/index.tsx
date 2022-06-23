82a61041ab09c24897146b58d4acaeae3cc30f4f
Jonatas Santos <jonatassantos96@hotmail.com>
2022-06-22 21:56:25 -0300

    ✨  Create header on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,35 @@
-import React, { useState } from "react";
-import { Container } from "./styles";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components/native";
+
+import happyEmoji from "@assets/happy.png";
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+} from "./styles";
 
 const Home = () => {
-  return <Container></Container>;
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name={"logout"} size={24} color={COLORS.TITLE} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
 };
 
 export default Home;
