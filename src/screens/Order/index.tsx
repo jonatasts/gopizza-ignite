e73c5def64a0421ee9439b358b64b40773fe4504
Jonatas Santos <jonatassantos96@hotmail.com>
2022-07-01 22:23:27 -0300

    ✨  Create order screen

---
+++ src/screens/Order/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Platform } from "react-native";
+
+import ButtonBack from "@components/ButtonBack";
+import RadioButton from "@components/RadioButton";
+import Input from "@components/Input";
+
+import styles, {
+  Container,
+  ContentScroll,
+  Form,
+  Header,
+  Photo,
+  Sizes,
+  Title,
+  Label,
+  FormRow,
+  InputGroup,
+  Price,
+} from "./styles";
+import { PIZZA_TYPES } from "@utils/pizzaTypes";
+import Button from "@components/Button";
+
+const Order = () => {
+  const behavior = Platform.OS === "ios" ? "padding" : undefined;
+  const [size, setSize] = useState("");
+  return (
+    <Container behavior={behavior}>
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={styles.goBack} />
+        </Header>
+
+        <Photo source={{ uri: "https://github.com/jonatasts.png" }} />
+
+        <Form>
+          <Title>Nome da Pizza</Title>
+
+          <Label>Selecione o tamanho</Label>
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                selected={size === item.id}
+                onPress={() => setSize(item.id)}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Numero da mesa</Label>
+              <Input keyboardType={"numeric"} />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType={"numeric"} />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$ 00,00</Price>
+
+          <Button title={"Confirmar Pedido"} />
+        </Form>
+      </ContentScroll>
+    </Container>
+  );
+};
+
+export default Order;
