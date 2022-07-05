be28d5351d77e3bdcb6a7734a39540b19301f722
Jonatas Santos <jonatassantos96@hotmail.com>
2022-07-04 22:19:33 -0300

    feat: Save new order on firestore

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Platform } from "react-native";
+import { Alert, Platform } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import firestore from "@react-native-firebase/firestore";
 
 import ButtonBack from "@components/ButtonBack";
 import RadioButton from "@components/RadioButton";
 import Input from "@components/Input";
+import Button from "@components/Button";
 
 import styles, {
   Container,
@@ -21,7 +22,9 @@ import styles, {
   Price,
 } from "./styles";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
-import Button from "@components/Button";
+
+import { useAuth } from "@hooks/auth";
+
 import { ProductNavigationProps } from "src/@types/navigation";
 import { PizzaProps } from "./types";
 
@@ -31,16 +34,60 @@ const Order = () => {
   const [pizza, setPizza] = useState<PizzaProps>({} as PizzaProps);
   const [tableNumber, setTableNumber] = useState("");
   const [quantity, setQuantity] = useState(0);
+  const [sedingOrder, setSedingOrder] = useState(false);
 
   const navigation = useNavigation();
   const route = useRoute();
   const { id } = route.params as ProductNavigationProps;
+  const { user } = useAuth();
 
   const amount =
     size && !isNaN(quantity) ? pizza.prices_sizes[size] * quantity : "0,00";
 
   const goBack = () => {
     navigation.goBack();
+  };
+
+  const validateFields = (isLoading = false) => {
+    if (!size) {
+      return "Selecione o tamanho da Pizza!";
+    }
+
+    if (!tableNumber.trim()) {
+      return "Informe o numero da mesa!";
+    }
+
+    if (!quantity) {
+      return "Informe a quantidade!";
+    }
+
+    setSedingOrder(isLoading);
+    return "success";
+  };
+
+  const onOrder = () => {
+    const status = validateFields(true);
+
+    if (status == "success") {
+      firestore()
+        .collection("orders")
+        .add({
+          quantity,
+          amount,
+          pizza: pizza.name,
+          size,
+          table_number: tableNumber,
+          status: "Preparando",
+          waiter_id: user?.id,
+          image: pizza.photo_url,
+        })
+        .then(() => navigation.navigate("home"))
+        .catch(() =>
+          Alert.alert("Pedido", "Não foi possível realizar o pedido!")
+        );
+    } else {
+      return Alert.alert("Pedido", status);
+    }
   };
 
   useEffect(() => {
@@ -95,7 +142,11 @@ const Order = () => {
 
           <Price>Valor de R$ {amount}</Price>
 
-          <Button title={"Confirmar Pedido"} />
+          <Button
+            title={"Confirmar Pedido"}
+            onPress={onOrder}
+            isLoading={sedingOrder}
+          />
         </Form>
       </ContentScroll>
     </Container>
