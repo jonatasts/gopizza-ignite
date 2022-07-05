a07695ff29c91232e08886a1946ed41529b6aae0
Jonatas Santos <jonatassantos96@hotmail.com>
2022-07-04 23:13:18 -0300

    feat: Update orders quantity of bottom bar on real time

--- src/routes/user.tab.routes.tsx
+++ src/routes/user.tab.routes.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Platform } from "react-native";
 import { useTheme } from "styled-components/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import firestore from "@react-native-firebase/firestore";
 
 import Home from "@screens/Home";
 import Orders from "@screens/Orders";
 import BottomMenu from "@components/BottomMenu";
 
+import { useAuth } from "@hooks/auth";
+
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const UserTabRoutes = () => {
+  const [notifications, setNotifications] = useState("0");
+
   const { COLORS } = useTheme();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection("orders")
+      .where("status", "==", "Pronto")
+      .where("waiter_id", "==", user?.id)
+      .onSnapshot((snapshot) => setNotifications(String(snapshot.docs.length)));
+
+    return () => subscribe();
+  }, []);
+
   return (
     <Navigator
       screenOptions={{
@@ -38,7 +55,11 @@ const UserTabRoutes = () => {
         component={Orders}
         options={{
           tabBarIcon: ({ color }) => (
-            <BottomMenu title={"Pedidos"} color={color} notifications={"0"} />
+            <BottomMenu
+              title={"Pedidos"}
+              color={color}
+              notifications={notifications}
+            />
           ),
         }}
       ></Screen>
